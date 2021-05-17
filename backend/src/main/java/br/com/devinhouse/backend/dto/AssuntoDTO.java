8fd599d50958ff35c00e18cd9dd309e1a6b7957a
Cristian Ogliari <cogliari@gmail.com>
2021-05-17 20:59:30 -0300

    criacao: dtos `

---
+++ backend/src/main/java/br/com/devinhouse/backend/dto/AssuntoDTO.java
@@ -0,0 +1,5 @@
+package br.com.devinhouse.backend.dto;
+
+public class AssuntoDTO {
+//	Uso de mapeamento de ojeto relacional usando a especificação JPA;
+}
---
+++ backend/src/main/java/br/com/devinhouse/backend/dto/InteressadoDTO.java
@@ -0,0 +1,5 @@
+package br.com.devinhouse.backend.dto;
+
+public class InteressadoDTO {
+//	Uso de mapeamento de ojeto relacional usando a especificação JPA;
+}
