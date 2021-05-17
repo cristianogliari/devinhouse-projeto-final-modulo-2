f74de5673a225480fe82110cf8924f2d31954fab
Cristian Ogliari <cogliari@gmail.com>
2021-05-17 20:49:15 -0300

    criacao: services

---
+++ backend/src/main/java/br/com/devinhouse/backend/services/InteressadoService.java
@@ -0,0 +1,7 @@
+package br.com.devinhouse.backend.services;
+
+public class InteressadoService {
+//	7 - Não poderá ser cadastrado um novo interessado com um id já existente;
+//	8 - Não poderá ser cadastrado um novo interessado com um mesmo documento de indentificação;
+//	9 - Não poderá ser cadastrado um novo interessado com um documento de identificação inválido;
+}
---
+++ backend/src/main/java/br/com/devinhouse/backend/services/ProcessoService.java
@@ -0,0 +1,11 @@
+package br.com.devinhouse.backend.services;
+
+public class ProcessoService {
+//	1 - Não poderá ser cadastrado um novo processo com um id já existente;
+//	2 - Não poderá ser cadastrado um novo processo com uma chave de processo já existente;
+//	3 - Não poderá ser cadastrado um novo processo com interessados inativos;
+//	4 - Não poderá ser cadastrado um novo processo com assuntos inativos;
+//	5 - Não poderá ser cadastrado um novo processo com interessados inesistentes no sistema;
+//	6 - Não poderá ser cadastrado um novo processo com assuntos inesistentes no sistema;
+//	10 - A formatação da chave do processo deve seguir a seguinte mascara SGORGAOSETOR NUPROCESSO/NUANO ex: SOFT 1/2021;
+}
