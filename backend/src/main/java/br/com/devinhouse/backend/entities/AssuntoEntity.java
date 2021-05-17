78ba5c449e51ae480555aa0381076f61e6c8294b
Cristian Ogliari <cogliari@gmail.com>
2021-05-17 20:42:51 -0300

    criacao: entidades

---
+++ backend/src/main/java/br/com/devinhouse/backend/entities/AssuntoEntity.java
@@ -0,0 +1,9 @@
+package br.com.devinhouse.backend.entities;
+
+public class AssuntoEntity {
+//	id -> int - notnull - primarykey
+//	descricao -> varchart(250) -> notnull
+//	dtcadastro -> date -> notnull
+//	flativo -> char(1) -> notnull
+
+}
---
+++ backend/src/main/java/br/com/devinhouse/backend/entities/ProcessoEntity.java
@@ -0,0 +1,15 @@
+package br.com.devinhouse.backend.entities;
+
+public class ProcessoEntity {
+//	id -> int -> not null - primary key
+//	nuprocesso -> int -> notnull
+//	sgorgaosetor -> char(4) -> notnull
+//	nuano -> char(4) -> notnull
+//	chaveprocesso -> char(50) -> notnull
+//	descricao -> char(250) -> notnull
+//	@OneToOne
+//	cdassunto -> int -> notnull -> foreign key
+//	@ManyToOne
+//	cdinteressado -> int -> notnull -> foreign key	
+	
+}
