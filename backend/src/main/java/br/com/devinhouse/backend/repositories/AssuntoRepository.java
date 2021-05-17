153c6f98e81bf4a7511665e7078ad1b440ba0bda
Cristian Ogliari <cogliari@gmail.com>
2021-05-17 20:56:43 -0300

    criacao: repositories

---
+++ backend/src/main/java/br/com/devinhouse/backend/repositories/AssuntoRepository.java
@@ -0,0 +1,5 @@
+package br.com.devinhouse.backend.repositories;
+
+public class AssuntoRepository {
+//	Uso de mapeamento de ojeto relacional usando a especificação JPA;
+}
---
+++ backend/src/main/java/br/com/devinhouse/backend/repositories/InteressadoRepository.java
@@ -0,0 +1,5 @@
+package br.com.devinhouse.backend.repositories;
+
+public class InteressadoRepository {
+//	Uso de mapeamento de ojeto relacional usando a especificação JPA;
+}
