afc422c169508a8d3a34df494625d5ea6a213a5e
Cristian Ogliari <cogliari@gmail.com>
2021-05-18 19:19:23 -0300

    criacao: repositorio assunto com JPA

--- backend/src/main/java/br/com/devinhouse/backend/repositories/AssuntoRepository.java
+++ backend/src/main/java/br/com/devinhouse/backend/repositories/AssuntoRepository.java
@@ -1,5 +1,11 @@
 package br.com.devinhouse.backend.repositories;
 
-public class AssuntoRepository {
-//	Uso de mapeamento de ojeto relacional usando a especificação JPA;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.devinhouse.backend.entities.Assunto;
+
+@Repository
+public interface AssuntoRepository extends JpaRepository<Assunto, Integer> {
+	//	Uso de mapeamento de ojeto relacional usando a especificação JPA;
 }
