da0687aff00a2bfd404c7aa71f513c7b74974428
Cristian Ogliari <cogliari@gmail.com>
2021-05-20 19:31:43 -0300

    criacao: processo service, TODO regras de negocio de cadastro

--- backend/src/main/java/br/com/devinhouse/backend/services/ProcessoService.java
+++ backend/src/main/java/br/com/devinhouse/backend/services/ProcessoService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import br.com.devinhouse.backend.entities.Assunto;
 import br.com.devinhouse.backend.entities.Interessado;
 import br.com.devinhouse.backend.entities.Processo;
+import br.com.devinhouse.backend.repositories.AssuntoRepository;
+import br.com.devinhouse.backend.repositories.InteressadoRepository;
 import br.com.devinhouse.backend.repositories.ProcessoRepository;
 
 @Service
@@ -16,9 +18,42 @@ public class ProcessoService {
 	@Autowired
 	private ProcessoRepository repository;
 	
+	@Autowired
+	private AssuntoRepository assuntoRepository;
+	
+	@Autowired
+	private InteressadoRepository interessadoRepository;
+	
+	private boolean verificaExistenciaDeProcesso(Integer id) {
+		Processo processoEncontrado = repository.findById(id).get();				
+		boolean status = false;
+		
+		if(processoEncontrado.getId() > 0) {
+			status = true;
+		}
+		return status;
+	}
+	
 //	1 - Deverá haver um endpoint para criação de um processo;
 	public Processo cadastrarProcesso(Processo obj) {
+		Assunto assuntoLocalizado = assuntoRepository.findById(obj.getCdassunto().getId()).get();
+		obj.setCdassunto(assuntoLocalizado);
+		
+		Interessado interessadoLocalizado = interessadoRepository.findById(obj.getCdinteressado().getId()).get();
+		obj.setCdinteressado(interessadoLocalizado);
+
+		//		10 - A formatação da chave do processo deve seguir a seguinte mascara SGORGAOSETOR NUPROCESSO/NUANO ex: SOFT 1/2021;
+		obj.setChaveprocesso(obj.getSgorgaosetor() + " " + obj.getNuprocesso() + "/" + obj.getNuano());
+				
 		return repository.save(obj);
+
+		// 		TODO Regras de negócio	
+		//		2 - Não poderá ser cadastrado um novo processo com uma chave de processo já existente;
+		//		3 - Não poderá ser cadastrado um novo processo com interessados inativos;
+		//		4 - Não poderá ser cadastrado um novo processo com assuntos inativos;
+		//		5 - Não poderá ser cadastrado um novo processo com interessados inexistentes no sistema;
+		//		6 - Não poderá ser cadastrado um novo processo com assuntos inexistentes no sistema;
+
 	}
 		
 //	2 - Deverá haver um endpoint para listagem de todos os processos, retornando todos os atributos de cada processo;
@@ -48,11 +83,34 @@ public class ProcessoService {
 	
 //	7 - Deverá haver um endpoint para atualização de todos os atributos de um processo baseado na sua identificação única (ID);
 	public Processo atualizarProcessoPorID(Integer id, Processo obj) {
-		Processo processoAtualizado = repository.findById(id).get();
+		Processo processoEncontrado = repository.findById(id).get();
 		
-		// TODO processos atributos atualização
+		if (verificaExistenciaDeProcesso(id)) {
+			if(!obj.getDescricao().isBlank()) {
+				processoEncontrado.setDescricao(obj.getDescricao());
+			}
+			if(!obj.getSgorgaosetor().isBlank()) {
+				processoEncontrado.setSgorgaosetor(obj.getSgorgaosetor());
+			}
+			if(!obj.getNuano().isBlank()) {
+				processoEncontrado.setNuano(obj.getNuano());
+			}
+			if(!obj.getChaveprocesso().isBlank()) {
+				processoEncontrado.setChaveprocesso(obj.getChaveprocesso());
+			}
+			if(!obj.getCdassunto().getDescricao().isBlank()) {
+				Assunto assuntoAtualizadoEncontrado = assuntoRepository.findById(obj.getCdassunto().getId()).get();
+				processoEncontrado.setCdassunto(assuntoAtualizadoEncontrado);
+			}
+			if(!obj.getCdinteressado().getNminteressado().isBlank()) {
+				Interessado interessadoAtualizadoEncontrado = interessadoRepository.findById(obj.getCdinteressado().getId()).get();
+				processoEncontrado.setCdinteressado(interessadoAtualizadoEncontrado);
+			}
+		} else {
+			throw new RuntimeException("Processo não encontrado");
+		}
 		
-		return repository.save(processoAtualizado);
+		return repository.save(processoEncontrado);
 	}
 	
 //	8 - Deverá haver um endpoint para exclusão de um processo baseado na sua identificação única (ID);
@@ -62,13 +120,4 @@ public class ProcessoService {
 		
 		return repository.findAll();
 	}
-	
-// TODO Regras de negócio	
-//	1 - Não poderá ser cadastrado um novo processo com um id já existente;
-//	2 - Não poderá ser cadastrado um novo processo com uma chave de processo já existente;
-//	3 - Não poderá ser cadastrado um novo processo com interessados inativos;
-//	4 - Não poderá ser cadastrado um novo processo com assuntos inativos;
-//	5 - Não poderá ser cadastrado um novo processo com interessados inesistentes no sistema;
-//	6 - Não poderá ser cadastrado um novo processo com assuntos inesistentes no sistema;
-//	10 - A formatação da chave do processo deve seguir a seguinte mascara SGORGAOSETOR NUPROCESSO/NUANO ex: SOFT 1/2021;
 }
