02dc0a7f670eea461c6c4d5c91a27e241246bca7
Cristian Ogliari <cogliari@gmail.com>
2021-05-23 23:18:56 -0300

    criacao: endpoint para busca de todos os assuntos e inclusa situacao de ativo padrao no cadastro

--- backend/src/main/java/br/com/devinhouse/backend/services/AssuntoService.java
+++ backend/src/main/java/br/com/devinhouse/backend/services/AssuntoService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import br.com.devinhouse.backend.entities.Assunto;
 import br.com.devinhouse.backend.repositories.AssuntoRepository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -15,7 +17,12 @@ public class AssuntoService {
 	private AssuntoRepository repository;
 	
 	public Assunto cadastrarAssunto(Assunto assunto) {
+		assunto.setFlativo("S");
 		return repository.save(assunto);
+	}
+	
+	public List<Assunto> buscarTodosOsAssuntos() {
+		return repository.findAll();
 	}
 	
 	public Assunto buscarAssuntoPeloId(Integer id) {
