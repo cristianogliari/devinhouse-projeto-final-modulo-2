b903ff98abf2f7f336596a33fc0ef624ad26bd4b
Cristian Ogliari <cogliari@gmail.com>
2021-05-26 14:44:55 -0300

    correcao: teste cadastrar interessado

--- backend/src/test/java/br/com/devinhouse/backend/service/InteressadoServiceTeste.java
+++ backend/src/test/java/br/com/devinhouse/backend/service/InteressadoServiceTeste.java
@@ -1,32 +1,64 @@
 package br.com.devinhouse.backend.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.devinhouse.backend.entities.Interessado;
+import br.com.devinhouse.backend.repositories.InteressadoRepository;
 import br.com.devinhouse.backend.services.InteressadoService;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@WebAppConfiguration
+@ExtendWith(MockitoExtension.class)
 public class InteressadoServiceTeste {
 	
-	@Autowired
-	private InteressadoService serviceImpl;
+	private static final Interessado interessado = new Interessado(1, "Fulano", "44431256040", new Date(), "S");
+	
+	@Mock 
+	private InteressadoRepository interessadoRepository;
+	
+	@InjectMocks
+	private InteressadoService interessadoService;
 	
 	@Test
-	public void buscarInteressadoPeloIdTeste() {
-		Interessado interessado = serviceImpl.buscarInteressadoPeloId(1);
-		assertEquals("1", String.valueOf(interessado.getId()));
-		assertEquals("1995-05-18 21:00:00", interessado.getDtnascimento());
-		assertEquals("S", interessado.getFlativo());
-		assertEquals("Bruno Zardo", interessado.getNminteressado());
-		assertEquals("08503736977", interessado.getNuidentificacao());
+	void cadastrarInteressadoTest() {
+		// where
+		interessadoService.cadastrarInteressado(interessado);
+		
+		// then
+		ArgumentCaptor<Interessado> interessadoArgumentCaptor = ArgumentCaptor.forClass(Interessado.class);
+		
+		verify(interessadoRepository).save(interessadoArgumentCaptor.capture());
+		
+		Interessado capturedInteressado = interessadoArgumentCaptor.getValue();
+		
+		assertThat(capturedInteressado).isEqualTo(interessado);
 	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
