d177af7f46f80360db7617a20e2f8ac4b7495b6d
Cristian Ogliari <cogliari@gmail.com>
2021-05-26 22:40:54 -0300

    correcao: metodo busca pelo assunto id, buscar todos os assuntos e teste de excecoes

--- backend/src/test/java/br/com/devinhouse/backend/service/AssuntoServiceTest.java
+++ backend/src/test/java/br/com/devinhouse/backend/service/AssuntoServiceTest.java
@@ -1,43 +1,27 @@
 package br.com.devinhouse.backend.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.junit.After;
-import org.junit.Before;
 
 import br.com.devinhouse.backend.services.AssuntoService;
 import br.com.devinhouse.backend.entities.Assunto;
 import br.com.devinhouse.backend.repositories.AssuntoRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-//@DataJpaTest
-//@SpringBootTest
-//@RunWith(SpringRunner.class)
-//@WebAppConfiguration
 @ExtendWith(MockitoExtension.class)
 public class AssuntoServiceTest {
 
@@ -50,7 +34,7 @@ public class AssuntoServiceTest {
 	private AssuntoService assuntoService;
 
 	@Test
-	public void cadastrarAssuntoTest() {
+	void cadastrarAssuntoTest() {
 		// given
 		assuntoService.cadastrarAssunto(assunto);
 
@@ -64,26 +48,44 @@ public class AssuntoServiceTest {
 		// then
 		assertThat(capturedAssunto).isEqualTo(assunto);
 	}
-
+	
 	@Test
-	@Disabled
-	public void buscarAssuntoPeloIdTest() {
-//		Assunto assunto = serviceImpl.buscarAssuntoPeloId(1);
-//		assertEquals("Roubo", assunto.getDescricao());
-//		assertEquals(1, assunto.getId());
-
-		int expected = 1;
-
-		System.out.println(assunto.getId());
-
-//		when(assuntoRepository.findById(expected)).thenReturn(Optional.of(mock(Assunto.class)));
-//
-//		serviceImpl.buscarAssuntoPeloId(expected);
-//		
-//
-//		verify(assuntoRepository).findById(expected);
+	void buscarTodosOsAssuntosTest() {
+		// given
 		
-		assertEquals(expected, assunto.getId());
+		//when
+		assuntoService.buscarTodosOsAssuntos();
+		
+		// then
+		verify(assuntoRepository).findAll();
 	}
 
+	@Test
+	void buscarAssuntoPeloIdTest() {
+		// given
+		List<Assunto> assuntoLista = new ArrayList<Assunto>();
+		assuntoLista.add(assunto);
+		int expected = 1;
+		when(assuntoRepository.findById(expected)).thenReturn(Optional.of(assunto));
+		when(assuntoRepository.findAll()).thenReturn(assuntoLista);
+		
+		// when
+		assuntoService.buscarAssuntoPeloId(expected);
+				
+		// then
+		verify(assuntoRepository).findById(expected);
+	}
+	
+	@Test 
+	void buscarAssuntoPeloIdTestThrowRunTimeException() {
+		// given
+		int expected = 1;
+		
+		// when
+		
+		// then
+		assertThatThrownBy(() -> assuntoService.buscarAssuntoPeloId(expected))
+			.isInstanceOf(RuntimeException.class)
+			.hasMessageContaining("Assunto não localizado.");
+	}
 }
